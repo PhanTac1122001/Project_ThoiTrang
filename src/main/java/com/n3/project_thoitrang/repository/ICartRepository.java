c855630dbfa87018af9afb5aa2965b12beee0b97
PhanTac1122001 <tintert1703@gmail.com>
2024-08-14 11:06:07 +0700

    ShopingCart

---
+++ src/main/java/com/n3/project_thoitrang/repository/ICartRepository.java
@@ -0,0 +1,15 @@
+package com.n3.project_thoitrang.repository;
+
+import com.n3.project_thoitrang.model.entity.Shoping_Cart;
+
+import java.util.List;
+
+public interface ICartRepository {
+    List<Shoping_Cart> findAll();
+
+    void deleteById(Integer id);
+
+    void updateCart(Shoping_Cart shopingCart);
+
+    Shoping_Cart findById(Integer id);
+}
